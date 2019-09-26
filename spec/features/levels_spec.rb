6fcb9100944fc21c6f94b95cdbf67f89f2185e88
Jelani Woods <jelani@firstdraft.com>
2019-09-26 10:08:40 -0500

    tests run, i guess needed devise

--- spec/features/levels_spec.rb
+++ spec/features/levels_spec.rb
@@ -2,24 +2,24 @@ require "rails_helper"
 
 describe "Query: Movie.all" do
   it "should list movie rows" do
-    # pending
+    pending("something else getting finished")
   end
 end
 
 describe "Query: Director.all" do
   it "should list director rows" do
-    # pending
+    pending("something else getting finished")
   end
 end
 
 describe "Query: Actor.all" do
   it "should list actor rows" do
-    # pending
+    pending("something else getting finished")
   end
 end
 
 describe "Query: Role.all" do
   it "should list role rows" do
-    # pending
+    pending("something else getting finished")
   end
 end
