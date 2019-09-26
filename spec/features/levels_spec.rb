f742e8700ada0f66087b0b38a11730dd0d0ce181
Jelani Woods <jelani@firstdraft.com>
2019-09-26 09:40:18 -0500

    Begin to add tests

---
+++ spec/features/levels_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Query: Movie.all" do
+  it "should list movie rows" do
+    # pending
+  end
+end
+
+describe "Query: Director.all" do
+  it "should list director rows" do
+    # pending
+  end
+end
+
+describe "Query: Actor.all" do
+  it "should list actor rows" do
+    # pending
+  end
+end
+
+describe "Query: Role.all" do
+  it "should list role rows" do
+    # pending
+  end
+end
